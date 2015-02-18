f5964a84d4479f1056ca35ab7f67112dc7a3aebb
gconen <gregconen@gmail.com>
2015-02-18 08:40:25 +0000

    add secure passwords to users and validate user data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+    before_save { self.email = email.downcase }
+    validates :name, presence: true, length: {maximum: 99}
+    VALID_EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d.\-]+\.[a-z]+\Z/i
+    validates :email, presence: true, length: {maximum: 254}, format: { with: VALID_EMAIL_REGEXP }, uniqueness: {case_sensitive: false}
+    validates :password, length: {minimum: 6}
+    has_secure_password
 end
