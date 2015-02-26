917b927ce7d9c856e521b5a001f17f893dcb9d4e
gconen <gregconen@gmail.com>
2015-02-26 18:52:18 +0000

    add flash message for successful sign-up

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,13 +2,25 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
    test "invalid users don't enter database" do
-    assert_no_difference 'User.count' do
-       get signup_path
-       post users_path, user: {name: "  ", email: 'test#test.com', 
+        assert_no_difference 'User.count' do
+            get signup_path
+            post users_path, user: {name: "  ", email: 'test#test.com', 
                                 password: "password",
                                 password_confirmation: "password"
                                 }
-    end
-    assert_template 'users/new'
+        end
+        assert_template 'users/new'
    end
+   
+   test "valid users enter database" do
+        assert_difference 'User.count', 1 do
+            get signup_path
+            post_via_redirect users_path, user: {name: "name", email: 'test@test.com', 
+                                password: "password",
+                                password_confirmation: "password"
+                                }
+        end
+        assert_template 'users/show'
+   end
+   
 end
