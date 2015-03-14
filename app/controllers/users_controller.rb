c8083aa61940cc702129772f5365fa432e184c0a
gconen <gregconen@gmail.com>
2015-03-14 21:29:18 +0000

    slightly change update page to finally work better

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,14 +30,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       #do things
     else
-      render edit
+      render 'edit'
     end
   end
+
   
   private
   
