728b8d0fee8116f5ee3b3532ee78fd950b856630
gconen <gregconen@gmail.com>
2015-04-09 01:36:00 +0000

    add picture uploads to microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,19 @@
 class Micropost < ActiveRecord::Base
     belongs_to :user
+    
+    mount_uploader :picture, PictureUploader
+    
     validates :user_id, presence: true
     validates :content, presence: true, length: {maximum: 140}
+    validates :picture_size
+    
     default_scope -> {order(created_at: :desc)}
+    
+    
+    def picture_size
+        if picture.size > 5.megabytes
+            errors.add(:picture, "should be less than 5MB")
+        end
+    end
 
 end
